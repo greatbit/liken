960430a1e19c8cbccff8c93e6d69fc95d8db8601
Aziz Namazov <anamazov@amazon.com>
2019-08-14 21:58:03 +1000

    Add pager mock

--- ui/src/src/launches/Launches.js
+++ ui/src/src/launches/Launches.js
@@ -14,6 +14,12 @@ import $ from 'jquery';
 class Launches extends Component {
     state = {
         launches: [],
+        pager: {
+            total: 0,
+            current: 0,
+            maxVisiblePage: 7,
+            itemsOnPage: 20
+        },
         loading: true
     };
     constructor(props) {
@@ -22,7 +28,7 @@ class Launches extends Component {
     }
 
     componentDidMount() {
-//        this.getLaunches();
+        this.getLaunches();
         this.interval = setInterval(this.getLaunches, 30000);
     }
 
@@ -40,6 +46,17 @@ class Launches extends Component {
         }, this);
     }
 
+    getProgressBar(launch){
+        return (
+            <div class="progress">
+              <div class="progress-bar progress-bar-striped" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.RUNNING, launch.launchStats.total)}></div>
+              <div class="progress-bar bg-success" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.PASSED, launch.launchStats.total)}></div>
+              <div class="progress-bar bg-danger" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.FAILED, launch.launchStats.total)}></div>
+              <div class="progress-bar bg-warning" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.BROKEN, launch.launchStats.total)}></div>
+            </div>
+        )
+    }
+
 
     render() {
         return (
@@ -55,7 +72,51 @@ class Launches extends Component {
                        loading={this.state.loading}
                      />
                    </div>
-                  Please refer to the list of launches in your source test management system
+                  <table class="table table-striped">
+                        <thead>
+                            <tr>
+                              <th scope="col">Title</th>
+                              <th scope="col">Progress</th>
+                              <th scope="col">Created</th>
+                              <th scope="col">Started</th>
+                              <th scope="col">Finished</th>
+                              <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {
+                              this.state.launches.map(function(launch){
+                                  return (
+                                         <tr>
+                                             <td>
+                                                  <Link to={'/launch/' + launch.id}>
+                                                      {launch.name}
+                                                  </Link>
+                                             </td>
+                                             <td>{this.getProgressBar(launch)}</td>
+                                             <td>{Utils.timeToDate(launch.createdTime)}</td>
+                                             <td>{Utils.timeToDate(launch.startTime)}</td>
+                                             <td>{Utils.timeToDate(launch.finishTime)}</td>
+                                             <td>
+                                                  {launch.launcherConfig && launch.launcherConfig.launcherId &&
+                                                      <FontAwesomeIcon icon={faPlug}/>
+                                                  }
+                                             </td>
+                                         </tr>
+                                         );
+                              }.bind(this))
+                        }
+                        </tbody>
+                    </table>
+                    <div>
+                        <Pager
+                            totalItems={this.state.pager.total}
+                            currentPage={this.state.pager.current}
+                            visiblePages={this.state.pager.maxVisiblePage}
+                            itemsOnPage={this.state.pager.itemsOnPage}
+                            onPageChanged={this.handlePageChanged}
+                        />
+                    </div>
               </div>
               <Footer/>
           </div>
