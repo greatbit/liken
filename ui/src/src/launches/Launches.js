d7b605c5edfd8ffbb303a6c6cd1f56382c14db37
Aziz Namazov <anamazov@amazon.com>
2019-08-16 00:46:15 +1000

    Add pager view

--- ui/src/src/launches/Launches.js
+++ ui/src/src/launches/Launches.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
 import Alerts from '../common/Alerts';
+import Pager from '../common/Pager';
 import { withRouter } from 'react-router';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 import * as Utils from '../common/Utils';
 import { FadeLoader } from 'react-spinners';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import $ from 'jquery';
 
@@ -14,49 +16,75 @@ import $ from 'jquery';
 class Launches extends Component {
     state = {
         launches: [],
+        filter: {
+            skip: 0,
+            limit: 20
+        },
         pager: {
             total: 0,
             current: 0,
             maxVisiblePage: 7,
-            itemsOnPage: 20
+            itemsOnPage: 20,
+            current: 0
         },
         loading: true
     };
     constructor(props) {
         super(props);
         this.getLaunched = this.getLaunches.bind(this);
+        this.handlePageChanged = this.handlePageChanged.bind(this);
     }
 
     componentDidMount() {
-        this.getLaunches();
-        this.interval = setInterval(this.getLaunches, 30000);
+        Utils.queryToFilter(this.props.location.search.substring(1));
+        this.getLaunches(this);
+        this.interval = setInterval(() => (this.getLaunches(this)), 30000);
     }
 
-    getLaunches(){
+    getLaunches(that){
         axios
-            .get("/api/launch")
+            .get("/api/launch?" + Utils.filterToQuery(this.state.filter) )
             .then(response => {
-                 this.state.launches = response.data;
-                 this.state.loading = false;
-                 this.setState(this.state);
+                 that.state.launches = response.data.content;
+                 that.state.pager.total = response.data.totalElements;
+                 that.state.pager.current = response.data.number;
+                 that.state.pager.itemsOnPage = response.data.size;
+                 this.state.pager.current =  this.state.filter.skip / this.state.filter.limit;
+                 that.state.loading = false;
+                 that.setState(that.state);
         }).catch(error => {
             Utils.onErrorMessage("Couldn't get launches: ", error);
-            this.state.loading = false;
-            this.setState(this.state);
-        }, this);
+            that.state.loading = false;
+            that.setState(that.state);
+        });
     }
 
     getProgressBar(launch){
         return (
-            <div class="progress">
-              <div class="progress-bar progress-bar-striped" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.RUNNING, launch.launchStats.total)}></div>
-              <div class="progress-bar bg-success" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.PASSED, launch.launchStats.total)}></div>
-              <div class="progress-bar bg-danger" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.FAILED, launch.launchStats.total)}></div>
-              <div class="progress-bar bg-warning" role="progressbar" style={Utils.getProgressBarStyle(launch.launchStats.statusCounters.BROKEN, launch.launchStats.total)}></div>
+            <div className="progress">
+              <div className="progress-bar progress-bar-striped" role="progressbar" style={Utils.getProgressBarStyle(this.getTestcasesCountByStatus(launch, 'RUNNING'), launch.testcases.length)}></div>
+              <div className="progress-bar bg-success" role="progressbar" style={Utils.getProgressBarStyle(this.getTestcasesCountByStatus(launch, 'PASSED'), launch.testcases.lengthl)}></div>
+              <div className="progress-bar bg-danger" role="progressbar" style={Utils.getProgressBarStyle(this.getTestcasesCountByStatus(launch, 'FAILED'), launch.testcases.length)}></div>
+              <div className="progress-bar bg-warning" role="progressbar" style={Utils.getProgressBarStyle(this.getTestcasesCountByStatus(launch, 'BROKEN'), launch.testcases.length)}></div>
             </div>
         )
     }
 
+    getTestcasesCountByStatus(launch, status){
+        return launch.testcases.filter(testcase => testcase.status == status).length;
+    }
+
+    handlePageChanged(newPage) {
+        this.state.pager.current = newPage;
+        this.state.filter.skip = newPage * this.state.pager.itemsOnPage;
+        this.getLaunches(this);
+        this.setState(this.state);
+        this.updateUrl();
+    }
+
+    updateUrl(){
+        this.props.history.push("/" + this.props.match.params.project + '/launches?' + Utils.filterToQuery(this.state.filter));
+    }
 
     render() {
         return (
@@ -71,8 +99,8 @@ class Launches extends Component {
                        color={'#135f38'}
                        loading={this.state.loading}
                      />
-                   </div>
-                  <table class="table table-striped">
+                 </div>
+                  <table className="table table-striped">
                         <thead>
                             <tr>
                               <th scope="col">Title</th>
@@ -80,7 +108,6 @@ class Launches extends Component {
                               <th scope="col">Created</th>
                               <th scope="col">Started</th>
                               <th scope="col">Finished</th>
-                              <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -97,11 +124,6 @@ class Launches extends Component {
                                              <td>{Utils.timeToDate(launch.createdTime)}</td>
                                              <td>{Utils.timeToDate(launch.startTime)}</td>
                                              <td>{Utils.timeToDate(launch.finishTime)}</td>
-                                             <td>
-                                                  {launch.launcherConfig && launch.launcherConfig.launcherId &&
-                                                      <FontAwesomeIcon icon={faPlug}/>
-                                                  }
-                                             </td>
                                          </tr>
                                          );
                               }.bind(this))
